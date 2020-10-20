5167342d3c582d47a745decce6c0467da61e68ed
Letícia Pacheco dos Santos <le.pacheco0310@hotmail.com>
2020-10-19 22:39:40 -0300

    minions

---
+++ json.js
@@ -0,0 +1,12 @@
+const json = {"data": [{"letra": "A", "sounds": "da_da_da_papoe.mp3", "imagem": "aaa.jpg"},
+                      {"letra": "L", "sounds": "sirene.56", "imagem": "lll.png"},
+                      {"letra": "J", "sounds": "exclam.mp3", "imagem": "jjj.png"},
+                      {"letra": "R", "sounds": "para_tu.mp3", "imagem": "rrr.png"},
+                      {"letra": "V", "sounds": "ta_da.mp3", "imagem": "vvv.png"},
+                      {"letra": "P", "sounds": "yay.mp3", "imagem": "ppp.png"},
+                      {"letra": "X", "sounds": "banana.mp3", "imagem": "xxx.png"},
+                      {"letra": "Y", "sounds": "aaahhh.mp3", "imagem": "yyy.png"},
+                      {"letra": "Z", "sounds": "hello.mp3", "imagem": "zzz.png"}]};
+
+                    
+
